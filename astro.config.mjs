35e69822b32fbf4c2853e85fa64753ad20bfe3c1
Cris Correa <cristina.correa.segade@gmail.com>
2025-05-08 04:03:06 +0100

    fixed astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config'
+import tailwind from "@astrojs/tailwind"
+
+import robotsTxt from "astro-robots-txt"
 
 export default defineConfig({
   site: 'https://CrisCorreaS.github.io',
